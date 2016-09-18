5557c196cc8ec1a567e5b25352a71572b3750e6a
cmoraes <celinomoraes@hotmail.com>
2016-09-18 22:23:11 +0200

    + outils de copie, et 'split'

--- chaine-de-caracteres/outils_chaine.cpp
+++ chaine-de-caracteres/outils_chaine.cpp
@@ -41,4 +41,99 @@ namespace outils_chaine {
 
 		return nombre_voyelles;
 	}
+
+	int nombreOccurences(const char *chaine, const char c)
+	{
+		int nombre_occurences = 0;
+		int taille_chaine = tailleChaine(chaine);
+		for (int i = 0; i < taille_chaine; i++) {
+			if (chaine[i] == c) nombre_occurences++;
+		}
+
+		return nombre_occurences;
+	}
+
+	char* copier(const char* source) {
+		int taille = tailleChaine(source);
+		char* dest = new char(taille + 1);
+		for (int i = 0; i <= taille;i++) {
+			dest[i] = source[i];
+		}
+
+		return dest;
+	}
+
+	char * copier(const char* source, const unsigned int pos)
+	{
+		int taille = tailleChaine(source);
+		if (pos <= taille) taille = pos;
+		char* dest = new char(taille + 1);
+		for (int i = 0; i < taille;i++) {
+			dest[i] = source[i];
+		}
+		dest[taille] = '\0';
+
+		return dest;
+
+	}
+
+	char * copier(const char *source, const unsigned int debut, const unsigned int fin)
+	{
+		int taille = tailleChaine(source);
+		int idx_from, idx_to;
+		if (debut > fin) {
+			idx_from = fin;
+			idx_to = debut;
+		}
+		else {
+			idx_from = debut;
+			idx_to = fin;
+		}
+
+		if (idx_to >= taille) { idx_to = taille; }
+		if (idx_from >= taille) { idx_from = taille; }
+
+		int taille_dest = (idx_to - idx_from) + 1; // +1 Pour le '\0'
+
+		char* dest = new char(taille_dest);
+		for (int i = idx_from; i <= idx_to;i++) {
+			dest[i - idx_from] = source[i];
+		}
+
+		dest[taille_dest - 1] = '\0';
+
+		return dest;
+	}
+
+	char** separerChaine(const char* chaine, const char c) {
+		int nombre_occurences = 0;
+		int taille_chaine = tailleChaine(chaine);
+		for (int i = 0; i < taille_chaine; i++) {
+			if (chaine[i] == c) nombre_occurences++;
+		}
+
+		// Si le caractère n'est pas dans la chaîne on renvoie la chaîne entière
+		if (nombre_occurences == 0) {
+			char** ret = new char*[1];
+			ret[0] = copier(chaine);
+			return ret;
+		}
+		else {
+			char** ret = new char*[nombre_occurences + 1];
+			int nombre_ajouts = 0;
+			int previous_pos = 0;
+
+			for (int i = 0; i < taille_chaine; i++) {
+				if (chaine[i] == c) {
+					ret[nombre_ajouts] = copier(chaine, previous_pos, i);
+					nombre_ajouts++;
+					previous_pos = i + 1;
+				}
+				ret[nombre_ajouts] = copier(chaine, previous_pos, taille_chaine);
+			}
+
+			return ret;
+		}
+
+	}
 }
--- chaine-de-caracteres/outils_chaine.h
+++ chaine-de-caracteres/outils_chaine.h
@@ -13,4 +13,21 @@ namespace outils_chaine {
 
 	// Retourne le nombre de voyelles d'une chapine de caractère
 	int nombreVoyelles(const char*);	
+
+	// Retourne le nombre d'occurences d'un caractère donné:
+	int nombreOccurences(const char*, const char);
+
+	//Copie une chaîne vers une autre
+	char* copier(const char*);
+
+	//Copie une chaîne jusqu'à un indice donné
+	char* copier(const char*, const unsigned int);
+
+	//Copie une chaîne à partir et jusqu'à un indice donnée
+	char* copier(const char*, const unsigned int, const unsigned int);
+	
+	//Retoure un tableau de chaînes de caractères 
+	//correspondant à la séparation de la chaine
+	//selon un caractère donné
+	char** separerChaine(const char*, const char);
 }
